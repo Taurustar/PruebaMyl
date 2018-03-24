fa69a3b3c83388c486f481d4d4c21ff323b32281
Taurustar <scarreno@dark-ware.com>
2018-03-24 04:51:57 -0300

    Escena 1 Comenzada y con ambientacion lista. Faltan dialogos y preguntas. Escena principal Comenzada, falta carga de niveles asincrona.

---
+++ PruebaMyl/Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPrincipal : MonoBehaviour {
+
+    public static MenuPrincipal instance;
+    public GameObject canvasCargar;
+    public GameObject loadSceneBar;
+
+    void awake()
+    {
+        if(!instance)
+        {
+            instance = this;
+        }
+
+        if(this != instance)
+        {
+            Destroy(this);
+        }
+
+        DontDestroyOnLoad(instance);
+    }
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowLoadBar(int levelToGo)
+    {
+        canvasCargar.GetComponent<Canvas>().enabled = true;
+        StartCoroutine(FillTheBar(levelToGo));
+    }
+
+    IEnumerator FillTheBar(int levelToGo)
+    {
+        yield return new WaitForSeconds(1);
+        loadSceneBar.GetComponent<UnityEngine.UI.Image>().transform.localScale.Set(20,1,1);
+        yield return new WaitForSeconds(1);
+        loadSceneBar.GetComponent<UnityEngine.UI.Image>().transform.localScale.Set(40, 1, 1);
+        yield return new WaitForSeconds(1);
+        loadSceneBar.GetComponent<UnityEngine.UI.Image>().transform.localScale.Set(60, 1, 1);
+        yield return new WaitForSeconds(1);
+        loadSceneBar.GetComponent<UnityEngine.UI.Image>().transform.localScale.Set(80, 1, 1);
+        yield return new WaitForSeconds(1);
+        loadSceneBar.GetComponent<UnityEngine.UI.Image>().transform.localScale.Set(100, 1, 1);
+        yield return new WaitForSeconds(1);
+        UnityEngine.SceneManagement.SceneManager.SetActiveScene(UnityEngine.SceneManagement.SceneManager.GetSceneByBuildIndex(levelToGo));
+    }
+
+
+
+}
---
+++ PruebaMyl/Assets/Scripts/Scene1Scripts/RandomQuestion.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomQuestion : MonoBehaviour {
+
+    GameObject[] Questions;
+    GameObject[] texts;
+    int currentText;
+    int currentQuestion;
+
+    void Start()
+    {
+        currentQuestion = Random.Range(0, Questions.Length - 1);
+        currentText = 0;
+        texts[currentText].SetActive(true);
+    }
+
+
+    void QuestionChange()
+    {
+        currentText++;
+        if(currentText >= texts.Length - 1)
+        {
+            currentText = texts.Length - 1;
+            StartCoroutine(GoToQuestion());
+        }
+    }
+
+    IEnumerator GoToQuestion()
+    {
+        Questions[currentQuestion].SetActive(false);
+        yield return new WaitForEndOfFrame();
+        currentQuestion = Random.Range(0, Questions.Length - 1);
+        Questions[currentQuestion].SetActive(true);
+    }
+}
