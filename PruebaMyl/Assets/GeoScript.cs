af17bfe0347fbb56f8aa4c5e30465803f5c8d701
Taurustar <scarreno@dark-ware.com>
2018-03-25 18:26:00 -0300

    Pequeño avance del día antes de seguir con la escena 3

---
+++ PruebaMyl/Assets/GeoScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SimpleJson;
+
+public class GeoScript : MonoBehaviour {
+
+    string urlGeo = "https://maps.googleapis.com/maps/api/geocode/json?new_forward_geocoder=true&sensor=false&address=";
+    string geoKey = "&key=AIzaSyA6zF9_mD6H7gcRsTephnXF7_SVixuaeAE";
+    public string address;
+    public string latitude, longitude;
+    public InputField addressBox;
+
+    public void SendGeoRequest()
+    {
+        address = addressBox.text;
+        StartCoroutine(GeoRequest());
+    }
+
+
+    IEnumerator GeoRequest()
+    {
+        WWW www = new WWW(urlGeo + address + geoKey);
+        yield return www;
+        yield return new WaitUntil(()=> www.isDone);
+
+        string requestJson = JSON.Parse(www.text);
+
+        longitude = www.text;
+        Debug.Log(www.text);
+    }
+}
