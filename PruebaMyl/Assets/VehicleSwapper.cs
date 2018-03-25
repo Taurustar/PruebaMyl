086eff81418cf82ad83f149c1988cd97c9c6689b
Taurustar <scarreno@dark-ware.com>
2018-03-25 01:13:13 -0300

    -Escena 2 Lista (Necesita muy pequeños ajustes en el feeling de la iluminación y niebla)

---
+++ PruebaMyl/Assets/VehicleSwapper.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSwapper : MonoBehaviour {
+
+    public GameObject[] vehicles;
+    public GameObject[] sunLights;
+    public int vehicleIndex = 0;
+    public int sunIndex = 0;
+    public Material[] skyMaterials;
+
+
+    void Start()
+    {
+        vehicles[vehicleIndex].SetActive(true);
+        sunLights[sunIndex].SetActive(true);
+        RenderSettings.skybox = skyMaterials[sunIndex];
+        if (sunIndex == 1 || sunIndex == 3)
+        {
+            RenderSettings.fog = true;
+
+        }
+        else
+        {
+            RenderSettings.fog = false;
+        }
+        RenderSettings.sun = sunLights[sunIndex].GetComponent<Light>();
+    }
+
+	public void ChangeVehicleNext()
+    {
+        vehicles[vehicleIndex].SetActive(false);
+        vehicleIndex++;
+        if(vehicleIndex >= vehicles.Length)
+        {
+            vehicleIndex = 0;
+        }
+        if (vehicleIndex < 0)
+        {
+            vehicleIndex = vehicles.Length - 1;
+        }
+
+        vehicles[vehicleIndex].SetActive(true);
+    }
+    public void ChangeVehiclePrev()
+    {
+        vehicles[vehicleIndex].SetActive(false);
+        vehicleIndex--;
+        if (vehicleIndex >= vehicles.Length)
+        {
+            vehicleIndex = 0;
+        }
+        if (vehicleIndex < 0)
+        {
+            vehicleIndex = vehicles.Length - 1;
+        }
+        vehicles[vehicleIndex].SetActive(true);
+    }
+
+
+
+
+
+
+
+
+    public void ChangeSceneNext()
+    {
+        sunLights[sunIndex].SetActive(false);
+        
+        sunIndex++;
+        if (sunIndex >= sunLights.Length)
+        {
+            sunIndex = 0;
+        }
+        if (sunIndex < 0)
+        {
+            sunIndex = sunLights.Length - 1;
+        }
+
+        sunLights[sunIndex].SetActive(true);
+        RenderSettings.skybox = skyMaterials[sunIndex];
+        if(sunIndex == 1)
+        {
+            RenderSettings.fog = true;
+            
+        }
+        else
+        {
+            RenderSettings.fog = false;
+        }
+        RenderSettings.sun = sunLights[sunIndex].GetComponent<Light>();
+    }
+    
+}
